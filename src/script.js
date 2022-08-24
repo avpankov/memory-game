0ed3451dcfda10c587be6c06d6faf580ff2c0190
Artyom Pankov <apankov4696@gmail.com>
2022-08-24 22:14:58 +0300

    feat:complete script

--- src/script.js
+++ src/script.js
@@ -1,10 +1,13 @@
 let cards = document.querySelectorAll('.card');
 let isFlipped = false;
+let isLocked = false;
 let firstCard, secondCard;
 
 cards.forEach(card => card.addEventListener('click', flip));
 
 function flip() {
+    if (isLocked) return;
+    if (this === firstCard) return;
     this.classList.add('flip');
 
     if (!isFlipped) {
@@ -14,7 +17,6 @@ function flip() {
     }
 
     secondCard = this;
-    isFlipped = false;
 
     checkForMatch();
 }
@@ -24,17 +26,32 @@ function checkForMatch() {
         disableCards();
         return;
     }
+
     unflipCards();
 }
 
 function disableCards() {
     firstCard.removeEventListener('click', flip);
     secondCard.removeEventListener('click', flip);
+    reset();
 }
 
 function unflipCards() {
+    isLocked = true;
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
+        reset();
     }, 1000);
 }
+
+function reset() {
+    [isFlipped, isLocked] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffleCards() {
+    cards.forEach(card => {
+        card.style.order = Math.round(Math.random() * 12);
+    })
+})()
